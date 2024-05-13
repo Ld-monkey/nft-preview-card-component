2f34442eac6ec5397d12fdf75b62d4db3004bb5b
Ld-monkey <lfourteau91@gmail.com>
2024-05-13 15:42:58 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'soft-blue': 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+        'bg-very-dark-blue': 'hsl(217, 54%, 11%)',
+        'card-very-dark-blue': 'hsl(216, 50%, 16%)',
+        'line-very-dark-blue': 'hsl(215, 32%, 27%)',
+      },
+    },
   },
   plugins: [],
 };
