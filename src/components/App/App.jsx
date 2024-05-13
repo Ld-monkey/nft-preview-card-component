32d899e3dd4570a2a04b3ad3448d0ba60d6ef1b7
Ld-monkey <lfourteau91@gmail.com>
2024-05-13 15:43:32 +0200

    simple html structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,28 @@
 function App() {
   return (
-    <div>
-      <p className="text-2xl text-red-500 font-bold underline">Hello World</p>
+    <div className="h-screen w-full bg-bg-very-dark-blue">
+      <main>
+        <article>
+          <img src="" alt="" />
+          <header>
+            <h1>Equilibrium #3429</h1>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
+          </header>
+          <div>
+            <ul>
+              <li>0.041 ETH</li>
+              <li>3 days left</li>
+            </ul>
+          </div>
+          <hr />
+          <footer>
+            <img src="" alt="" />
+            <p>
+              Creation of <span>Jules Wyvern</span>
+            </p>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
